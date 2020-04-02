9e188efff4b08aefe219266db673a5ad2b97614d
Administrator <12112>
2020-04-02 21:12:48 +0800

    菜单管理，百度翻译

---
+++ src/main/java/util/ConstantApi.java
@@ -0,0 +1,9 @@
+package util;
+
+public class ConstantApi {
+    /*百度翻译的api*/
+    // 在平台申请的APP_ID 详见 http://api.fanyi.baidu.com/api/trans/product/desktop?req=developer
+    public static final String BaiduFanyi_APP_ID = "20200401000410414";/*appid*/
+    public static final String BaiduFanyi_SECURITY_KEY = "W2CAjWMy5w27m4HNlAwT";/*秘钥*/
+
+}
